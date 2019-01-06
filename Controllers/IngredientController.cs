987a371a544d7f0f37dec1654c40f00a50cdf7f8
Mattias Norell <mattias.norell@gmail.com>
2019-01-06 11:54:12 +0100

    Added controllers

---
+++ Controllers/IngredientController.cs
@@ -0,0 +1,33 @@
+﻿using Feedbag.Business.Providers;
+using Feedbag.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Feedbag.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IngredientController : ControllerBase
+    {
+        private readonly IIngredientProvider ingredientProvider;
+        
+        public IngredientController(
+            IIngredientProvider ingredientProvider
+        ){
+            this.ingredientProvider = ingredientProvider;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] UpdateIngredientDto ingredientDto)
+        {
+            this.ingredientProvider.Save(ingredientDto);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.ingredientProvider.Delete(id);
+        }
+    }
+}
