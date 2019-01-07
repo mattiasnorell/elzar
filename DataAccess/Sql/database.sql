22d326aeee95da146ffa60fa77c1c5172d0cffb4
Mattias Norell <mattias.norell@gmail.com>
2019-01-07 14:46:41 +0100

    Update to database script

--- DataAccess/Sql/database.sql
+++ DataAccess/Sql/database.sql
@@ -1,23 +1,25 @@
-CREATE TABLE `Recipes` (
-                    `Id`	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-                    `Title`	TEXT(100),
-                    `Image`	TEXT(255),
-                    `Description`	TEXT(500),
-                    `SourceUrl` TEXT(255),
-                    `CreatedAtUtc`	INTEGER,
-                    `UpdatedAtUtc`	INTEGER
-                    )
-
 CREATE TABLE `HowToSteps` (
-                    `Id`	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-                    `RecepieId` INTEGER,
-                    `Step`	TEXT,
-                    )
+	`Id`	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`RecipeId`	INTEGER,
+	`Step`	TEXT
+);
 
 CREATE TABLE `Ingredients` (
-                    `Id`	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-                    `RecepieId` INTEGER,
-                    `Amount`	TEXT(100),
-                    `Unit`	TEXT(100),
-                    `Name`	TEXT(100)
-                    )
+	`Id`	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`RecipeId`	INTEGER,
+	`Amount`	TEXT ( 100 ),
+	`Unit`	TEXT ( 100 ),
+	`Name`	TEXT ( 100 )
+);
+
+CREATE TABLE `Recipes` (
+	`Id`	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`Title`	TEXT ( 100 ),
+	`Image`	TEXT ( 255 ),
+	`Description`	TEXT ( 500 ),
+	`SourceUrl`	TEXT ( 255 ),
+	`Tags`	TEXT,
+	`CreatedAtUtc`	TEXT,
+	`UpdatedAtUtc`	TEXT
+);
+
